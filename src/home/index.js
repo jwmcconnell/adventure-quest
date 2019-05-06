3c9b17ee985430f0907120d4c7d2016b6b6c622f
Jackmc <jackmc1122@gmail.com>
2019-05-06 16:56:03 -0700

    Save user.

--- src/home/index.js
+++ src/home/index.js
@@ -1,4 +1,5 @@
 import makeUser from './make-user.js';
+import api from '../services/api.js';
 
 const userForm = document.getElementById('user-form');
 
@@ -6,5 +7,5 @@ userForm.addEventListener('submit', (event) => {
   event.preventDefault();
   const formData = new FormData(userForm);
   const user = makeUser(formData);
-  console.log(user);
+  api.signUp(user);
 });
