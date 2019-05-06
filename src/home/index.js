627b5d1c313b3e427d30e730b6573a18dc1066c8
Jackmc <jackmc1122@gmail.com>
2019-05-06 16:57:50 -0700

    Add a map page and link to it upon form submission.

--- src/home/index.js
+++ src/home/index.js
@@ -5,7 +5,11 @@ const userForm = document.getElementById('user-form');
 
 userForm.addEventListener('submit', (event) => {
   event.preventDefault();
+
   const formData = new FormData(userForm);
   const user = makeUser(formData);
+
   api.signUp(user);
+
+  window.location = '../../src/map/map.html';
 });
