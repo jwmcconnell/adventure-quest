b4306cf9dd1ee5df9ac4eec74581c0be1ac54034
Jackmc <jackmc1122@gmail.com>
2019-05-08 11:24:52 -0700

    Fill the quest list with quests and remove the static link.

--- src/map/map.js
+++ src/map/map.js
@@ -1,3 +1,14 @@
 import loadProfile from '../load-profile.js';
+import api from '../services/api.js';
+import createQuestLink from './create-quest-link.js';
 
 loadProfile();
+
+const nav = document.getElementById('quests');
+
+const quests = api.getQuests();
+
+for(let i = 0; i < quests.length; i++) {
+  let link = createQuestLink(quests[i]);
+  nav.appendChild(link);
+}
