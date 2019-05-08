46e34a64adea95d2e287ccd97e7ad2618dec58ed
Jackmc <jackmc1122@gmail.com>
2019-05-08 11:16:34 -0700

    Move loadProfile into its own module.

--- src/map/map.js
+++ src/map/map.js
@@ -1,17 +1,3 @@
-import api from '../services/api.js';
+import loadProfile from '../load-profile.js';
 
-const username = document.getElementById('username');
-const userClass = document.getElementById('class');
-const health = document.getElementById('health');
-const gold = document.getElementById('gold');
-
-const user = api.getUser();
-
-if(!user) {
-  window.location = '../../';
-}
-
-username.textContent = user.username;
-userClass.textContent = user.class;
-health.textContent = 'Health: ' + user.health;
-gold.textContent = 'Gold: ' + user.gold;
+loadProfile();
