c53a4c1254ac575881f67347ecffd9e0a9381aa0
Jackmc <jackmc1122@gmail.com>
2019-05-09 11:47:36 -0700

    Check if the user is dead or has completed all of the quests when they load the map page. If so take them to the end game page.

--- src/map/map.js
+++ src/map/map.js
@@ -2,6 +2,8 @@ import loadProfile from '../load-profile.js';
 import api from '../services/api.js';
 import createQuestLink from './create-quest-link.js';
 import createCompletedQuest from './create-completed-quest.js';
+import isDead from './is-dead.js';
+import hasCompletedAllQuests from './has-completed-all-quests.js';
 
 loadProfile();
 
@@ -9,6 +11,10 @@ const nav = document.getElementById('quests');
 
 const quests = api.getQuests();
 const user = api.getUser();
+
+if(isDead(user) || hasCompletedAllQuests(quests, user)) {
+  window.location = '../end/';
+}
 
 for(let i = 0; i < quests.length; i++) {
   let link = null;
