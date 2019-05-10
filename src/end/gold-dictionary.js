6b774a2c145aebb91e2eeeb9e7ff27d7c2fb55b5
Jackmc <jackmc1122@gmail.com>
2019-05-10 09:49:32 -0700

    Complete end game logic with dictionary lookups for result messages.

---
+++ src/end/gold-dictionary.js
@@ -0,0 +1,14 @@
+const goldDictionary = {
+  dead: {
+    poor: 'You do not leave much for your family and you are soon forgotten.',
+    wealthy: 'You leave enough money for your children to start school but are soon overwhelmed withs student loans they need to finish',
+    rich: 'Your family prospers for many generations due to all you earned on your quests'
+  },
+  alive: {
+    poor: 'You do not have enough money to live off of so you soon have to return to questing',
+    wealthy: 'You are able to retire from quests but you are unable to leave your children anything.',
+    rich: 'You and your children prosper from all of your hard work'
+  }
+};
+
+export default goldDictionary;
